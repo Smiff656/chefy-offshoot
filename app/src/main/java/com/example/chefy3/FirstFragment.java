0a909f13200ba6e79bb47160fd7bacc5f07d3fcf
smiff656 <johnson.smith6560@gmail.com>
2022-03-18 13:02:47 -0700

    all

---
+++ app/src/main/java/com/example/chefy3/FirstFragment.java
@@ -0,0 +1,62 @@
+package com.example.chefy3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.chefy3.databinding.FragmentFirstBinding;
+
+public class FirstFragment<findViewById> extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private Button button;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceStatebutton =findViewById(R.id.button_first) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+
+            private void openActivity2() {
+                Intent intent;
+                intent = new Intent(this, Activity3.class);
+                startActivity(intent);
+            }
+        });
+        return null;
+    }
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
